9c6df80e3894bff78d0632d39a57d87499bf8e17
Ivan Bowen <bowenivan16@gmail.com>
2023-01-19 13:38:01 +0300

    add navbar.js

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+   return (
+      <nav className="nav">
+         <p className="center">🗺️ my traveljournal</p>
+      </nav>
+   )
+}
+
+export default Navbar
