e0dc5dd73cf1fc1b78c2ba838325056cd4ea7b2e
Ivan Bowen <bowenivan16@gmail.com>
2023-01-19 13:37:33 +0300

    import components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import Journal from "./components/Journal";
+import Navbar from "./components/Navbar";
+import { data } from "./data";
 
 function App() {
+   const journals = data.map((d) => {
+      return(
+         <Journal
+            key={d.id}
+            {...d} 
+         />
+      )
+   })
    return(
       <div>
-         Hello
+         <Navbar />
+         <section className="journal">
+            {journals}
+         </section>
       </div>
    );
 }
